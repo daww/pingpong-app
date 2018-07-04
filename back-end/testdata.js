85a9bad2c93e76d6042d1ca22a2f618a5b1791ea
Julian Koornneef <juliankoornneef@gmail.com>
2018-07-04 14:29:51 +0200

    Improved testdata to also play some matches

--- back-end/testdata.js
+++ back-end/testdata.js
@@ -32,46 +32,72 @@ callback = (error, response, body) => {
   }
 };
 
-// USER_DATA.forEach((item, index) => {
-//   request(
-//     {
-//       method: 'POST',
-//       url: 'http://localhost:3000/register',
-//       headers: {
-//         'Content-Type': 'application/json',
-//       },
-//       json: USER_DATA[index],
-//     },
-//     callback,
-//   );
-// });
-
-request('http://localhost:3000/users', { json: true }, (err, res, body) => {
-  if (err) {
-    return console.log(err);
-  }
-  const ids = body.map(user => user._id);
-
+USER_DATA.forEach((item, index) => {
   request(
     {
       method: 'POST',
-      url: 'http://localhost:3000/registermatch',
+      url: 'http://localhost:3000/register',
       headers: {
         'Content-Type': 'application/json',
       },
-      json: {
-        playerOneId: ids[0],
-        playerTwoId: ids[3],
-        games: [
-          {
-            playerOneScore: 11,
-            playerTwoScore: 6,
-          },
-        ],
-      },
+      json: USER_DATA[index],
     },
     callback,
   );
 });
+
+setTimeout(() => {
+  request('http://localhost:3000/users', { json: true }, (err, res, body) => {
+    if (err) {
+      return console.log(err);
+    }
+    const ids = body.map(user => user._id);
+
+    request(
+      {
+        method: 'POST',
+        url: 'http://localhost:3000/registermatch',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        json: {
+          playerOneId: ids[0],
+          playerTwoId: ids[3],
+          games: [
+            {
+              playerOneScore: 11,
+              playerTwoScore: 6,
+            },
+          ],
+        },
+      },
+      callback,
+    );
+    setTimeout(() => {
+      request(
+        {
+          method: 'POST',
+          url: 'http://localhost:3000/registermatch',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          json: {
+            playerOneId: ids[1],
+            playerTwoId: ids[2],
+            games: [
+              {
+                playerOneScore: 11,
+                playerTwoScore: 4,
+              },
+            ],
+          },
+        },
+        callback,
+      );
+      
+    }, 2000);
+  });
+}, 5000);
+
 
 // @TODO get users and do some test matches
