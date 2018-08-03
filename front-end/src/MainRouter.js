cb87b4c91fe7cc600e80f75b584fe16bf93a213f
Julian Koornneef <juliankoornneef@gmail.com>
2018-08-03 15:35:02 +0200

    Update MainRouter.js

--- front-end/src/MainRouter.js
+++ front-end/src/MainRouter.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import Rankings from "./pages/Rankings";
 import DuoRankings from "./pages/DuoRankings";
 import Matches from "./pages/Matches";
@@ -11,43 +11,20 @@ import NewMatch from "./pages/NewMatch";
 export default class MainRouter extends Component {
   render() {
     return (
-      <div>
-        <Route
-          path="/newmatch"
-          isAuthenticated={this.props.isAuthenticated}
-          render={() => <NewMatch userId={this.props.userId} />}
-        />
-        <Route
-          path="/rankings"
-          isAuthenticated={this.props.isAuthenticated}
-          component={Rankings}
-        />
-        <Route
-          path="/duorankings"
-          isAuthenticated={this.props.isAuthenticated}
-          component={DuoRankings}
-        />
+      <Switch>
+        <Route path="/newmatch" exact isAuthenticated={this.props.isAuthenticated} render={() => <NewMatch userId={this.props.userId} />} />
+        <Route path="/rankings" exact isAuthenticated={this.props.isAuthenticated} component={Rankings} />
+        <Route path="/duorankings" exact isAuthenticated={this.props.isAuthenticated} component={DuoRankings} />
         <Route
           path="/matches"
+          exact
           isAuthenticated={this.props.isAuthenticated}
           render={() => <Matches userData={this.props.userData} />}
         />
-        <Route
-          path="/user/:id"
-          isAuthenticated={this.props.isAuthenticated}
-          component={UserDetail}
-        />
-        <Route
-          path="/match/:id"
-          isAuthenticated={this.props.isAuthenticated}
-          component={MatchDetail}
-        />
-        <Route
-          path="/preferences/:id"
-          isAuthenticated={this.props.isAuthenticated}
-          component={Preferences}
-        />
-      </div>
+        <Route path="/user/:id" isAuthenticated={this.props.isAuthenticated} component={UserDetail} />
+        <Route path="/match/:id" isAuthenticated={this.props.isAuthenticated} component={MatchDetail} />
+        <Route path="/preferences/:id" isAuthenticated={this.props.isAuthenticated} component={Preferences} />
+      </Switch>
     );
   }
 }
